589a2979ac8c39d058815ec387fe23e254b2b299
Dhiraj Kshirsagar <dhirajkshirsagar000@gmail.com>
2026-03-13 17:39:36 +0530

    Create Exp4Selection.java

---
+++ Exp4Selection.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Exp4Selection {
+    public static void selectionSort(int[] arr) {
+        int i, j;
+        for (i = 0; i < arr.length - 1; i++) {
+            int min = i;
+            for (j = i; j < arr.length ; j++) {
+                if (arr[min] > arr[j]) {
+                    min = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+            System.out.print((i + 1) + " th pass :- ");
+            for (int k = 0; k < arr.length; k++) {
+                System.out.print(arr[k] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array : ");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        System.out.println("enter elements use space for separet ");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        selectionSort(arr);
+    }
+}
